f46ef83bde2381be1a48104503e596428ea79b5d
Tadele Kelayu <tadelsaloove@gmail.com>
2024-08-04 10:22:29 +0300

    Migragtions applied

--- music/views.py
+++ music/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from music.models import User
 # Create your views here.
+
+def index(request):
+    return render(request, 'music/index.html')
+
+def users(request):
+    user_list = User.objects.order_by('first_name')
+    user_dict = {'users': user_list}
+    return render(request, 'music/users.html', context= user_dict)
+
